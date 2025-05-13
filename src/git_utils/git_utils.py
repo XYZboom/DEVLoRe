ca2c55435a6ba7762fb51888159fc4bd6f596704
XYZboom <xyzboom@qq.com>
2025-05-13 12:53:11 +0800

    feat: swebench-lite prepare

---
+++ src/git_utils/git_utils.py
@@ -0,0 +1,21 @@
+import git
+from tqdm import tqdm
+
+
+class GitCloneProgress(git.remote.RemoteProgress):
+
+    def __init__(self, progress_bar):
+        super().__init__()
+        self.progress_bar = progress_bar
+
+    def update(self, op_code, cur_count, max_count=None, message=''):
+        if max_count is not None:
+            self.progress_bar.total = max_count
+            self.progress_bar.update(cur_count - self.progress_bar.n)
+
+
+def git_clone(url: str, to_path: str):
+    progress_bar = tqdm(total=100, unit='B', unit_scale=True)
+    # noinspection PyTypeChecker
+    git.Repo.clone_from(url, to_path, progress=GitCloneProgress(progress_bar))
+    progress_bar.close()
