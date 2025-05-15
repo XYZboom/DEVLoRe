741857c56723f98750061fbc6e0e63d0cc98ce1d
XYZboom <xyzboom@qq.com>
2025-05-15 20:10:47 +0800

    feat: related files for seaborn

--- src/swebench_utils/related_file/extract_related_file.py
+++ src/swebench_utils/related_file/extract_related_file.py
@@ -16,9 +16,12 @@ MAX_WORKERS = int(os.environ.get("MAX_WORKERS", default=16))
 
 def __do_extract(_pid, _bid):
     _path = os.path.join(SWEBENCH_LITE_PREPARE_PATH, f'bugs/{_pid}_{_bid}b')
-    _extract_path = os.path.join(SWEBENCH_LITE_PREPARE_PATH, 'related_files')
+    _extract_path = os.path.join(SWEBENCH_LITE_PREPARE_PATH, 'related_files', f'{_pid}_{_bid}b.txt')
+    if os.path.exists(_extract_path):
+        print(f'{_pid}_{_bid} related files are already extracted')
+        return
     _venv_py = f'{_path}_venv/bin/python'
-    if _pid != 'django':
+    if _pid not in ['django', 'seaborn']:
         print(f'does not support {_pid}')
     if _pid == 'django':
         _ori_argv = sys.argv
@@ -28,25 +31,34 @@ def __do_extract(_pid, _bid):
             return
         subprocess.run(
             [_venv_py, os.path.abspath(os.path.join(__file__, os.path.pardir, 'do_extract_related_file_django.py')),
-             _test_script, os.path.join(_extract_path, f'{_pid}_{_bid}b.txt'),
+             _test_script, _extract_path,
+             "-f", _path, os.path.join(TEMP_PATH, f'{_pid}_{_bid}b'),
+             "-args", *_failed_tests])
+    elif _pid == 'seaborn':
+        _ori_argv = sys.argv
+        _test_script = os.path.join(os.path.dirname(_venv_py), 'pytest')
+        _failed_tests = swe_failed_test(_pid, _bid)
+        if not _failed_tests:
+            return
+        subprocess.run(
+            [_venv_py, os.path.abspath(os.path.join(__file__, os.path.pardir, 'do_extract_related_file_seaborn.py')),
+             _test_script, _path, _extract_path,
              "-f", _path, os.path.join(TEMP_PATH, f'{_pid}_{_bid}b'),
              "-args", *_failed_tests])
 
 
 if __name__ == '__main__':
-    # for pid, bid in swe_pids_bids():
-    #     if pid != 'django' or bid != 47:
-    #         continue
-    #     __do_extract(pid, bid)
-    with concurrent.futures.ThreadPoolExecutor(
-            max_workers=MAX_WORKERS
-    ) as executor:
-        futures = [
-            executor.submit(
-                __do_extract,
-                pid,
-                bid
-            )
-            for pid, bid in swe_pids_bids()
-        ]
-        concurrent.futures.wait(futures)
+    for pid, bid in swe_pids_bids():
+        __do_extract(pid, bid)
+    # with concurrent.futures.ThreadPoolExecutor(
+    #         max_workers=MAX_WORKERS
+    # ) as executor:
+    #     futures = [
+    #         executor.submit(
+    #             __do_extract,
+    #             pid,
+    #             bid
+    #         )
+    #         for pid, bid in swe_pids_bids()
+    #     ]
+    #     concurrent.futures.wait(futures)
